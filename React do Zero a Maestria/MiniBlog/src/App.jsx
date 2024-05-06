8dc924ddc790e5ceeb5814ad026f518bcfa32cba
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-06 11:18:33 -0300

    styles: 💄 Navbar and Footer Style

--- React do Zero a Maestria/MiniBlog/src/App.jsx
+++ React do Zero a Maestria/MiniBlog/src/App.jsx
@@ -3,6 +3,10 @@ import "./App.css";
 // React Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Components
+import Navbar from "./pages/Components/Navbar";
+import Footer from "./pages/Components/Footer";
+
 // Pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -11,12 +15,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
