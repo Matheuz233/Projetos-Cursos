a870d4af28ffbe0ffdfe67015e6d7c2088d935b3
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-10 08:08:16 -0300

    feat: 🔨 Home Screen

--- React do Zero a Maestria/MiniBlog/src/pages/Home/Home.jsx
+++ React do Zero a Maestria/MiniBlog/src/pages/Home/Home.jsx
@@ -1,12 +1,41 @@
 // CSS
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
+
+// Hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
 
-export default Home
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.home}>
+      <h1>Veja os Nossos Posts mais Recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>POSTS</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/post/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
