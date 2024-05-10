f1da266810d010b420ca514d3fd7923a74e59328
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-10 10:58:16 -0300

    feat: 🔨 Post Component / Post Information

---
+++ React do Zero a Maestria/MiniBlog/src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // Tratar do Vazamento de Memória
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q;
+
+        // Busca
+
+        //Dashboard
+
+        q = await query(collectionRef, orderBy("createAt", "desc"));
+
+        await onSnapshot(q, (querySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      }
+
+      setLoading(false);
+    }
+
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {documents, loading, error};
+};
