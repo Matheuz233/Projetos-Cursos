690d0cfeb3174fda66621ed426470d1ba7cf40cf
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-13 11:10:09 -0300

    feat: 🔨 Dashboard Screen/Functions

--- React do Zero a Maestria/MiniBlog/src/hooks/useFetchDocuments.jsx
+++ React do Zero a Maestria/MiniBlog/src/hooks/useFetchDocuments.jsx
@@ -37,6 +37,12 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
             where("tagsArray", "array-contains", search),
             orderBy("createAt", "desc")
           );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createAt", "desc")
+          );
         } else {
           q = await query(collectionRef, orderBy("createAt", "desc"));
         }
@@ -58,7 +64,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
     }
 
     loadData();
-  }, [docCollection, documents, search, uid, cancelled]);
+  }, [docCollection, search, uid, cancelled]);
 
   useEffect(() => {
     return () => setCancelled(true);
