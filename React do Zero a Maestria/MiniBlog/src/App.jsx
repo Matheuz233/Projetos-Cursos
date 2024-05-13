4e9a72017f233ef9b8762b838b2db8667701511d
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-13 10:22:00 -0300

    feat: 🔨 Individual Posts

--- React do Zero a Maestria/MiniBlog/src/App.jsx
+++ React do Zero a Maestria/MiniBlog/src/App.jsx
@@ -23,6 +23,7 @@ import Register from "./pages/Register/Register";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 function App() {
   // Lógica de Chamada do Usário e Monitoramento de Usuário
@@ -51,6 +52,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
               {/* Bloqueando Páginas que Precisam de Autenticação */}
               <Route
                 path="/login"
